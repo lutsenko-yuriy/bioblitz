c3606f92dd2536cb7e54ef80eefc4938e4b2a9d5
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-12-24 22:42:02 +0100

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Yuriy Lutsenko on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
